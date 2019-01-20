45f7ac922f671ddbfe47fafded2624d61c7351ce
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-01-20 19:36:59 +0700

    11

--- subpage/imgroup.php
+++ subpage/imgroup.php
@@ -1,50 +1,117 @@
 <?php
 require 'server/server.php';
 
+function getToken($length) {
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet .= "0123456789";
+    $max = strlen($codeAlphabet); // edited
+    for ($i = 0; $i < $length; $i++) {
+        $token .= $codeAlphabet[random_int(0, $max - 1)];
+    }
+    return $token;
+}
 
 $q_sub = "SELECT * FROM `subject` order by `subject_id`";
-$re_sub = mysqli_query($con,$q_sub);
+$re_sub = mysqli_query($con, $q_sub);
 $i = 0;
 $option_sub = '<option hidden selected  value="">เลือกวิชา</option>';
-while ($row_sub =  mysqli_fetch_array($re_sub)) {
-    $option_sub.="<option>" . $row_sub['subject_id'] ." : ".$row_sub['subject_name']. "</option>";
+while ($row_sub = mysqli_fetch_array($re_sub)) {
+    $option_sub.="<option value = \"" . $row_sub['subject_id'] . "\">" . $row_sub['subject_id'] . " : " . $row_sub['subject_name'] . "</option>";
     $i++;
 }
 
-$q_location = "SELECT `name_location` FROM `location_table` order BY `name_location`";
-$re_location = mysqli_query($con,$q_location);
+$q_location = "SELECT `order`,`name_location` FROM `location_table` order BY `name_location`";
+$re_location = mysqli_query($con, $q_location);
 $j = 0;
 $option_location = '<option hidden selected  value="">สถานที่สอบ</option>';
-while ($row_location =  mysqli_fetch_array($re_location)) {
-    $option_location.="<option> ห้อง " . $row_location['name_location']. "</option>";
+while ($row_location = mysqli_fetch_array($re_location)) {
+    $option_location.='<option value = "' . $row_location['order'] . '"> ห้อง ' . $row_location['name_location'] . '</option>';
     $j++;
 }
 if (isset($_POST['tab_room'])) {
 
-    print_r($_POST['tab_room']);
-    echo "<br>";
-    print_r($_POST['tab_num']);
-    echo "<br>";
-    print_r($_POST['com_room']);
-    echo "<br>";
-    print_r($_POST['com_num']);
-    echo "<br>";
-    echo $_POST['term'];
-    echo "<br>";
-    echo $_POST['year'];
-    echo "<br>";
-    echo $_POST['sub'];
-    echo "<br>";
-    echo $_POST['group_exam'];
-    echo "<br>";
-    echo $_POST['s_time'];
-    echo "<br>";
-    echo $_POST['e_time'];
-    echo "<br>";
-    echo $_POST['date'];
-    echo "<br>";
-    echo $_POST['type_exam'];
-    echo "<br>";
+    $term = $_POST['term'];
+    $year = $_POST['year'];
+    $date = $_POST['date'];
+    $s_time = $_POST['s_time'];
+    $e_time = $_POST['e_time'];
+    $type_exam = $_POST['type_exam'];
+    // $file = $_FILES['file']['tmp_name'];
+    $detail_id = "DT" . getToken(10);
+    //insert detial
+    $q_detail = "INSERT INTO `detail`(`detail_id`, `term`, `year`, `day`, `time_start`, `time_end`, `type`) VALUES ( '$detail_id','$term','$year','$date','$s_time','$e_time','$type_exam')";
+    if ($re_detail = mysqli_query($con, $q_detail)) {
+
+        $sub = $_POST['sub'];
+        $group_exam = $_POST['group_exam'];
+        $i = 0;
+        $room[] = '';
+        foreach ($_POST['tab_room'] as $value) {
+            $room_id = "RD" . getToken(10);
+            $q_room = "INSERT INTO `room_detail`(`room_detail_id`, `room_id`, `detail_id`, `sub_id`, `sub_group`,`tool`) VALUES ('$room_id','$value','$detail_id','$sub','$group_exam','TABLET')";
+            if ($re_room = mysqli_query($con, $q_room)) {
+                $room[$i++] = $room_id;
+            } else {
+                header("Location: imgroup.php");
+                $_SESSION['alert'] = 4;
+                exit();
+            }
+        }
+        foreach ($_POST['com_room'] as $value) {
+            $room_id = "RD" . getToken(10);
+            $q_room = "INSERT INTO `room_detail`(`room_detail_id`, `room_id`, `detail_id`, `sub_id`, `sub_group`,`tool`) VALUES ('$room_id','$value','$detail_id','$sub','$group_exam','COMPUTER')";
+            if ($re_room = mysqli_query($con, $q_room)) {
+                $room[$i++] = $room_id;
+            } else {
+                header("Location: imgroup.php");
+                $_SESSION['alert'] = 4;
+                exit();
+            }
+        }
+
+        $i = 0;
+        $num[] = '';
+        foreach ($_POST['tab_num'] as $value) {
+            $num[$i] = $value;
+            $i++;
+        }
+        foreach ($_POST['com_num'] as $value) {
+            $num[$i] = $value;
+            $i++;
+        }
+
+        if (($handle = fopen("testcsv.csv", "r")) !== FALSE) {
+            $i = 0;
+            $student[] = '';
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+
+                $value = implode("','", $data);
+                $student[$i] = $value;
+                $i++;
+            }
+            $i = 0;
+            $s = 0;
+            foreach ($room as $a) {
+                $j = 1;
+                $cout = $num[$i];
+                while ($j <= $cout) {
+                    $sr_id = "SR" . getToken(10);
+                    $std = $student[$s++];
+                    $q_sub = "INSERT INTO `student_room`(`student_room_id`, `std_id`, `room_detail_id`, `seat`, `note`) VALUES ('$sr_id','$std','$a','$j','')";
+                    if ($re_sub = mysqli_query($con, $q_sub)) {
+                        $_SESSION['alert'] = 3;
+                    } else {
+                        header("Location: imgroup.php");
+                        $_SESSION['alert'] = 4;
+                        exit();
+                    }
+                    $j++;
+                }
+                $i++;
+            }
+        }
+    }
 }
 ?>
 
@@ -66,13 +133,17 @@ if (isset($_POST['tab_room'])) {
 
         <!-- Font Awesome CSS -->
         <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        
+
         <!-- Custom CSS -->
         <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
-        
+
         <link href="dist/bootstrap-clockpicker.min.css" rel="stylesheet" type="text/css" />
         <!-- BEGIN CSS for this page -->
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"/>
+
+        <!-- sweet alert2 -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.33.1/dist/sweetalert2.all.min.js"></script>
+
         <!-- END CSS for this page -->
 
     </head>
@@ -81,7 +152,7 @@ if (isset($_POST['tab_room'])) {
 
         <div id="main">
 
-            <?php require 'menu/navmenu.php'  ?>
+            <?php //require 'menu/navmenu.php'   ?>
 
 
             <div class="content-page"><!-- content-page -->
@@ -115,7 +186,7 @@ if (isset($_POST['tab_room'])) {
                                                 <div class="col-md-3">
                                                     <label for="year">ปีการศึกษา</label>
                                                     <select name="year" class="form-control select2" required>
-                                                    <option hidden selected  value="">เลือกปีการศึกษา</option>
+                                                        <option hidden selected  value="">เลือกปีการศึกษา</option>
                                                         <option>2561</option>
                                                         <option>2562</option>
                                                         <option>2563</option>
@@ -143,14 +214,14 @@ if (isset($_POST['tab_room'])) {
                                                 </div>
                                                 <div class="col-md-6">
                                                     <div class="form-group">
-                                                    <label for="time">เวลา เริ่ม - สิ้นสุด</label>
+                                                        <label for="time">เวลา เริ่ม - สิ้นสุด</label>
                                                         <div class ="row" >
                                                             <div class="col-md-5">
                                                                 <div class="input-group clockpicker" data-autoclose="true"  data-placement="left"  data-default = '00.00'>
                                                                     <input type="text" class="form-control" name = "s_time" placeholder = "เวลาเริ่มต้น" required >
-                                                                        <span class="input-group-addon">
-                                                                            <span class="glyphicon glyphicon-time"></span>
-                                                                        </span>
+                                                                    <span class="input-group-addon">
+                                                                        <span class="glyphicon glyphicon-time"></span>
+                                                                    </span>
                                                                 </div>
 
                                                             </div>
@@ -158,13 +229,13 @@ if (isset($_POST['tab_room'])) {
                                                             <div class="col-md-5">
                                                                 <div class="input-group clockpicker" data-autoclose="true"   data-placement="right"  data-default = '00.00'>
                                                                     <input type="text" class="form-control" name = "e_time" placeholder = "เวลาสิ้นสุด" required>
-                                                                        <span class="input-group-addon">
-                                                                            <span class="glyphicon glyphicon-time"></span>
-                                                                        </span>
+                                                                    <span class="input-group-addon">
+                                                                        <span class="glyphicon glyphicon-time"></span>
+                                                                    </span>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        
+
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6">
@@ -177,7 +248,7 @@ if (isset($_POST['tab_room'])) {
                                                     <div class="form-group">
                                                         <label for="type">ประเภท</label>
                                                         <select name = "type_exam" class="form-control select2" required>
-                                                        <option hidden selected  value="">เลือกประเภท</option>
+                                                            <option hidden selected  value="">เลือกประเภท</option>
                                                             <option>กลางภาค</option>
                                                             <option>ปลายภาค</option>
                                                             <option>แก้ไอ</option>	
@@ -187,7 +258,7 @@ if (isset($_POST['tab_room'])) {
                                                 </div>
                                             </div><!--end filter -->
                                             <div class="text-center"><!-- up file -->
-                                                <input class="btn btn-md" type="file">
+                                                <input class="btn btn-md" type="file" name = "file">
                                             </div><!--end up file -->
                                         </div>
                                     </div>
@@ -247,7 +318,7 @@ if (isset($_POST['tab_room'])) {
                                                     <div class="col-lg-6">
                                                         <div class="text-center">
                                                             <a class="btn btn-sm btn-info" id = "btn1"><i class="fa fa-plus"></i></a>
-                                                            
+
                                                             <a class="btn btn-sm btn-danger" id = "btn11"> X </a>
                                                         </div><br>
                                                     </div>
@@ -261,12 +332,12 @@ if (isset($_POST['tab_room'])) {
                                             </div><!--end buttom -->
                                             <div class="col-lg-12">
                                                 <div class="text-center">
-                                                <hr>
+                                                    <hr>
                                                     <button class="btn btn-xm btn-success text-center" id = "submit" type="submit">submit</button>
                                                     <br><br>
                                                 </div><!--end row all -->
                                             </div>
-                                            
+
                                         </div><!--end row all -->
                                     </div>
 
@@ -286,7 +357,7 @@ if (isset($_POST['tab_room'])) {
 
         </div>
         <!-- END main -->
-
+        <?php require '../alert.php'; ?>
         <script src="assets/js/modernizr.min.js"></script>
         <script src="assets/js/jquery.min.js"></script>
         <script src="assets/js/moment.min.js"></script>
@@ -315,36 +386,36 @@ if (isset($_POST['tab_room'])) {
 
         <script type="text/javascript" src="dist/bootstrap-clockpicker.min.js"></script>
         <script type="text/javascript">
-$('.clockpicker').clockpicker();
-</script>
+            $('.clockpicker').clockpicker();
+        </script>
         <script>
 
-                    $(document).ready(function () {
-            // data-tables
-            $('#example1').DataTable();
-                    $('.select2').select2();
-            });   
-        
-         </script>
+            $(document).ready(function () {
+                // data-tables
+                $('#example1').DataTable();
+                $('.select2').select2();
+            });
+
+        </script>
         <script>
 
                     $(document).ready(function(){
-                        var i = 1;
-                        var j = 1;
-                        var sum = '<?php echo $option_location; ?>'
-                            $("#btn1").click(function(){
-                                 $("#tablet_main").append(function(){
-                                        i++;
-                                        var tablet = '';
-                                        tablet += '<div class="row form-group" id = "tablet" ><div class="col-md-4 "> <label  for="room">' + i + '.ห้อง</label><select name="tab_room[]" class="form-control select2" required>' + sum + '</select></div><div class="col-md-4"></div><div class="col-md-4 "><label for="value">จำนวน</label><input  class="form-control" type="number" min="0" name = "tab_num[]" required></div></div>';
-                                        return tablet;
-                                 });
-                             });
-                            $("#btn2").click(function(){
-                                $("#computer_main").append(function(){
-                                     j++;
-                                    var computer = '';
-                                    computer += '<div class="row form-group" id ="computer" ><div class="col-md-4 "><label for="room">' + j + '.ห้อง</label><select name="com_room[]"  class="form-control select2" required>' + sum + '</select></div><div class="col-md-4"></div><div class="col-md-4 "><label for="value">จำนวน</label><input  class="form-control" type="number" min="0" name = "com_num[]" required ></div></div><!--end room & Value -->';
+            var i = 1;
+                    var j = 1;
+                    var sum = '<?php echo $option_location; ?>'
+                    $("#btn1").click(function(){
+            $("#tablet_main").append(function(){
+            i++;
+                    var tablet = '';
+                    tablet += '<div class="row form-group" id = "tablet" ><div class="col-md-4 "> <label  for="room">' + i + '.ห้อง</label><select name="tab_room[]" class="form-control select2" required>' + sum + '</select></div><div class="col-md-4"></div><div class="col-md-4 "><label for="value">จำนวน</label><input  class="form-control" type="number" min="0" name = "tab_num[]" required></div></div>';
+                    return tablet;
+            });
+            });
+                    $("#btn2").click(function(){
+            $("#computer_main").append(function(){
+        j++;
+                    var computer = '';
+                    computer += '<div class="row form-group" id ="computer" ><div class="col-md-4 "><label for="room">' + j + '.ห้อง</label><select name="com_room[]"  class="form-control select2" required>' + sum + '</select></div><div class="col-md-4"></div><div class="col-md-4 "><label for="value">จำนวน</label><input  class="form-control" type="number" min="0" name = "com_num[]" required ></div></div><!--end room & Value -->';
                                     return computer;
                                 });
                             });
