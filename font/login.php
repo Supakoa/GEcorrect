9345d5cf1995ff606582c16f89aab142f3efdc91
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-01-24 02:26:08 +0700

    sdsd

--- font/login.php
+++ font/login.php
@@ -3,22 +3,7 @@
   
   session_start();
   
-  $login_id = mysqli_real_escape_string($con,$_POST['text']);
-  $login_pw = mysqli_real_escape_string($con,$_POST['password']);
 
-  $sql = "SELECT * FROM student_table WHERE login_username=? AND login_password=?";
-  $stmt = mysqli_prepare($con,$sql);
-  mysqli_stmt_bind_param($stmt,"ss",$login_id,$login_pw);
-  mysqli_execute($stmt);
-  $result_user = mysqli_stmt_get_result($stmt);
-  $_SESSION['status'] = 0; //online
-  if ($result_user->num_rows >=1) {
-    $_SESSION['id'] = $login_id;
-    header("Location: main.php");
-  }else{
-    $_SESSION['status'] = 1; //not match
-    header("Location: index.php");
-  }
 
   //ReCAPTSHA
   $captcha;//ตัวแปร
@@ -27,6 +12,8 @@
   }
   if(!$captcha){
   echo '<h2>โปรดยืนยันตัวตนของคุณ</h2>';
+  $_SESSION['status'] = 1; //not match
+      header("Location: index.php");
   exit;
   }
   $secretKey = "6LedMIwUAAAAAB5C2WYf3tRWSNWpvXIOphG7JiVg";
@@ -34,8 +21,25 @@
   $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
   $responseKeys = json_decode($response,true);
   if(intval($responseKeys['success']) !== 1) {
+    $_SESSION['status'] = 1; //not match
+      header("Location: index.php");
   echo '<h2>โปรดทำการยันยืนให้ถูกต้อง</h2>';
   } else {
-  echo '<h2>ขอบคุณสำหรับคอมเม้น</h2>';
+
+    $login_id = mysqli_real_escape_string($con,$_POST['text']);
+
+    $sql = "SELECT * FROM student WHERE std_id=? ";
+    $stmt = mysqli_prepare($con,$sql);
+    mysqli_stmt_bind_param($stmt,"s",$login_id);
+    mysqli_execute($stmt);
+    $result_user = mysqli_stmt_get_result($stmt);
+    $_SESSION['status'] = 0; //online
+    if ($result_user->num_rows >=1) {
+      $_SESSION['id'] = $login_id;
+      header("Location: main.php");
+    }else{
+      $_SESSION['status'] = 1; //not match
+      header("Location: index.php");
+    }
   }
  ?>
