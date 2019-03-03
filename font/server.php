f85995dc32f4a74a88ccffa49c2cfbc74e1eaedf
supakoa <41123305+Supakoa@users.noreply.github.com>
2019-03-03 22:41:45 +0700

    koa

--- font/server.php
+++ font/server.php
@@ -1,4 +1,7 @@
 <?php
-  $con = mysqli_connect("localhost","root","P@ssw0rdGE","ge_exam") or die('เชื่อมต่อข้อมูลไม่สำเร็จ'. mysqli_connect_error());
+  // $con = mysqli_connect("localhost","root","P@ssw0rdGE","ge_exam") or die('เชื่อมต่อข้อมูลไม่สำเร็จ'. mysqli_connect_error());
+  // mysqli_set_charset($con,'utf8');
+
+  $con = mysqli_connect("localhost","root","","ge_exam") or die('เชื่อมต่อข้อมูลไม่สำเร็จ'. mysqli_connect_error());
   mysqli_set_charset($con,'utf8');
  ?>
